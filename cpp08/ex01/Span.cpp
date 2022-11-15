c903dbaa0766d0006845dd42459ca971ba66dde1
Abdoullah Kasmi <abkasmi@fu-r5-p4.s19.be>
2022-11-15 15:59:11 +0100

    cpp08

--- cpp08/ex01/Span.cpp
+++ cpp08/ex01/Span.cpp
@@ -5,9 +5,8 @@ Span::Span(unsigned int n): _n(n)
 	return ;
 }
 
-Span::Span(const Span& src)
+Span::Span(const Span& src): _n(src._n), _vector(src._vector)
 {
-	*this = src;
 	return ;
 }
 
@@ -32,14 +31,27 @@ void	Span::addNumber(unsigned int number)
 
 unsigned int	Span::shortestSpan(void) const
 {
-	if (this->_vector.size() <= 0)
+	if (this->_vector.size() <= 1)
 		throw NoDataException();
-	return *std::min_element(this->_vector.begin(), this->_vector.end());
+	else
+	{
+		std::vector<unsigned int> v2 = this->_vector;
+		std::sort(v2.begin(), v2.end());
+		unsigned int tmp = v2.at(1) - v2.at(0);
+		unsigned int size = v2.size();
+
+		for (unsigned int i = 1; i < size; i++)
+		{
+			if (tmp > v2[i + 1] - v2[i])
+				tmp = v2[i + 1] - v2[i];
+		}
+		return tmp;
+	}
 }
 
 unsigned int	Span::longestSpan(void) const
 {
-	if (this->_vector.size() <= 0)
+	if (this->_vector.size() <= 1)
 		throw NoDataException();
 	unsigned int max = *std::max_element(this->_vector.begin(), this->_vector.end());
 	unsigned int min = *std::min_element(this->_vector.begin(), this->_vector.end());
